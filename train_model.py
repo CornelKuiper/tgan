9278d06b79492f7d341f51b819951e8d50c70e5f
Danny Rogaar <dannyrogaar@gmail.com>
2017-11-30 15:40:38 +0100

    Added function interfacing to gensim/embeddings

--- train_model.py
+++ train_model.py
@@ -4,6 +4,8 @@ from donald import Trump, Processing
 training_data = Trump.train()
 training_tweets = Trump.column(training_data, 'Tweet_Text')		#grab a feature of all tweets
 
-Processing._load_embeddings()
-
-
+Processing.embeddings()
+Processing.get('king')
+Processing.nearest('king', 5)
+Processing.similarity('king', 'queen')
+Processing.nearest_tweets(['we must build a wall'], ['we shall make mexico pay for the wall'])
