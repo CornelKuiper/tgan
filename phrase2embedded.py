f5aa37f81f316134cac70ded467ec7886962ccaf
Danny Rogaar <dannyrogaar@gmail.com>
2017-12-12 10:55:12 +0100

    Added saving with a vocabulary.

--- phrase2embedded.py
+++ phrase2embedded.py
@@ -4,13 +4,22 @@ import numpy as np
 import nltk
 from pathlib import Path
 
-def save_embeddings(tweets_embeddings, name):
+def save_embeddings(tweets_embeddings, name, vocab = []):
     #save the output of convertphrases in a npz file. Each phrase is stored as a matrix of dim x nWords in the file.
     #an empty array is prepended to prevent numpy from trying to infer fixed shape and crashing
-    stitched_tweets = []
-    stitched_tweets.append([np.zeros(2)] + [np.stack(tweet, axis=-1) for tweet in tweets_embeddings])
+    stitched_tweets = [np.zeros(2)] + [np.stack(tweet, axis=-1) for tweet in tweets_embeddings]
 
-    np.savez(name, *stitched_tweets)
+    if vocab == []:
+        np.savez(name, *stitched_tweets)
+    else
+        #setup word -> embedding dictionary
+        phrase2embedding['dummy'] = stitched_tweets[0]
+        for idx in range(1, len(vocab)):
+            phrase2embedding[vocab[idx]] = stitched_tweets[idx]
+
+        np.savez(name, **phrase2embedding)
+
+
 
 def load_embeddings(name="./data/embeddings_trumptweets_train.npz", maxWords=45, padding=False):
     #reads multiple phrase matrices from file.
@@ -29,19 +38,19 @@ def load_embeddings(name="./data/embeddings_trumptweets_train.npz", maxWords=45,
     #load data
     loaded = np.load(name)                                  # close to avoid leaking file descriptor
     content = loaded[loaded.files[0]][1:]                   # get data, strip empty matrix
-    
+
     #throw away outlier length sentences
     if maxWords:
         mask = [phrase.shape[1] <= maxWords for phrase in content]
         content = content[mask]
-    
+
     #when set, pad phrases up to maxWords. (phrase.shape[0] == dimensionality)
     if padding:
         for phrase_idx in range(content.shape[0]):
             phrase = content[phrase_idx]
             dim = phrase.shape[0]
             nWords = phrase.shape[1]
-            
+
             content[phrase_idx] = np.concatenate([phrase, np.zeros((dim, maxWords - nWords))], axis=1)
 
     print("Loading embeddings complete.")
