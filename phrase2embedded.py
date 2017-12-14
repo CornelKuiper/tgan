996ff47d57aad8b0480876a544bbc253e9d017a8
Danny Rogaar <dannyrogaar@gmail.com>
2017-12-14 12:37:29 +0100

    merged convertphrases. added paddedtweet handle. factored out tokenisation. improved convertphrases

--- phrase2embedded.py
+++ phrase2embedded.py
@@ -7,11 +7,11 @@ from pathlib import Path
 def save_embeddings(tweets_embeddings, name, vocab = []):
     #save the output of convertphrases in a npz file. Each phrase is stored as a matrix of dim x nWords in the file.
     #an empty array is prepended to prevent numpy from trying to infer fixed shape and crashing
-    stitched_tweets = [np.zeros(2)] + [np.stack(tweet, axis=-1) for tweet in tweets_embeddings]
+    stitched_tweets = [np.zeros(2)] + [np.stack(tweet, axis=-1) for tweet in tweets_embeddings if len(tweet) > 0]
 
     if vocab == []:
         np.savez(name, *stitched_tweets)
-    else
+    else:
         #setup word -> embedding dictionary
         phrase2embedding['dummy'] = stitched_tweets[0]
         for idx in range(1, len(vocab)):
@@ -19,7 +19,8 @@ def save_embeddings(tweets_embeddings, name, vocab = []):
 
         np.savez(name, **phrase2embedding)
 
-
+def load_padded_tweet_embeddings():
+    return np.load("./data/tweet_embeddings.npy")
 
 def load_embeddings(name="./data/embeddings_trumptweets_train.npz", maxWords=45, padding=False):
     #reads multiple phrase matrices from file.
@@ -56,9 +57,18 @@ def load_embeddings(name="./data/embeddings_trumptweets_train.npz", maxWords=45,
     print("Loading embeddings complete.")
     return content
 
-def convertphrases(listofphrases):
-    #convert a list of phrases (a list of strings/sentences) to a list of lists of embeddings
+def asTokens(listofphrases):
+    #Given a list of phrases (strings) return a list of phrases (list of words)
+    return [nltk.word_tokenize(phrase) for phrase in listofphrases]
+
+def convertphrases(listofphrases, useGlove = False):
+    """
+    convert a list of phrases (a list of words) to a list of lists of embeddings
     #the result is preferably saved with save_embeddings and loaded similarly
+
+    for cornells tweet dataset, pass argument: list(np.load("D:/ML_datasets/tweet_tokens.npy"))
+    for trump twitter data, pass argument: asTokens(Trump.column(Trump.train()))
+    """
 
     def random_embedding():
         #Return random embedding. Possibly improve by weighing with frequency
@@ -66,24 +76,44 @@ def convertphrases(listofphrases):
         idx = randint(0, nwords)
         return Processing.get(Processing.embeddings().index2word[idx])
 
+    def parse(word):
+        #this should be consistent with special symbols used in embeddings such as <number>, <hashtag>
+        #currently; words in ignore return False
+        #otherwise, the correct word (s) is returned, belong to word
+        ignorelist = ['"', ',', '.', '!', '?', ':', 'of', 'and', 'a', '/', 'to', '(', ')', '...']
+
+        if word[0] == '#' or word[0] == '@':
+            return word[1:]
+        if word not in ignorelist:
+            return word
+        if "http" in word:
+            return word
+
+        return False
+
+    def setEmbedding(word):
+        word_embedding = None
+        hitsmiss = (0,0)
+        try:
+            word_embedding = Processing.get(word)
+            hitsmiss[0] += 1
+        except:
+            #if no word is found, insert a random embedding.
+            word_embedding = random_embedding()
+            hitsmiss[1] += 1
+        return (word_embedding, hitsmiss)
+
     embedded_phrases = []
     nHits = nMiss = 0
     for phrase in listofphrases:
-      phrase_embedding = []
-      for word in nltk.word_tokenize(phrase):
-        #set embeddings for each word
-        #if no word is found, insert a random embedding.
+        phrase_embedding = []
+        for word in phrase:
+            word = parse(word)                      #parse word
+            if not word: continue                   #ignore useless
 
-        word_embedding = None
-        try:
-          word_embedding = Processing.get(word)
-          nHits += 1
-        except:
-          word_embedding = random_embedding()
-          nMiss += 1
-        phrase_embedding.append(word_embedding)
-
-      embedded_phrases.append(phrase_embedding)
+            word_embedding = setEmbedding(word)
+            phrase_embedding.append(word_embedding)
+        embedded_phrases.append(phrase_embedding)
     print("Done finding the embeddings of all phrases & their words")
     print("During the search, I found ", nHits, " and was unable to embed ", nMiss, " words.")
     print("Random words have been inserted where no such embedding exists.")
